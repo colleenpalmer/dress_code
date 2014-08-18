75c015056da54019730111be795f79241e5458db
Colleen Palmer <colleen@instructure.com>
2014-08-18 16:30:38 -0600

    wip - bem

--- lib/dress_code/renderer.rb
+++ lib/dress_code/renderer.rb
@@ -5,12 +5,12 @@ require_relative '../dress_code'
 class DressCode::Renderer < Redcarpet::Render::HTML
 
   def block_code(code, language)
-    syntax = Pygments.highlight(code, {
+    syntax = Pygments.dresscode_highlight(code, {
       lexer: language,
       options: { encoding: 'utf-8' }
     })
     inner = if language == 'html'
-      "#{syntax} <div class='code-rendered'>#{code}</div>"
+      "#{syntax} <div class='dresscode_code-rendered'>#{code}</div>"
     else
       syntax
     end
